8672d261e27fc4bfb198a665fe5afbe97f87329b
Roshonia Brooks <roshoniabrooks@gmail.com>
2020-01-28 15:57:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,9 @@
 def speak_to_grandma(phrase)
-  if phrase != phrase ("I LOVE YOU GRANDMA!").upcase
-    return "I LOVE YOU TOO PUMPKIN!" 
-  elsif phrase != phrase.upcase()
+  if phrase != phrase.upcase()
     return "HUH?! SPEAK UP, SONNY!"
   else
     return "NO, NOT SINCE 1938!"
+  elsif   
   end
 end
   
